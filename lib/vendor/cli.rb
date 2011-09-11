290981a24e93876e1fb2decad0ee0bbf9dc3e7fa
keithpitt <me@keithpitt.com>
2011-09-11 22:33:34 +0800

    Some more refactoring to the CLI

--- lib/vendor/cli.rb
+++ lib/vendor/cli.rb
@@ -1,23 +1,7 @@
+require "thor"
+require "thor/group"
+require "ripl"
+
 require "vendor/cli/auth"
-
-module Vendor
-
-  module CLI
-
-    # This reload method kinda works. It works if you change/add methods, but if you
-    # remove methods from a class, they still seem to hang around. I believe there is
-    # a fair amount of magic in rails that lets you call a reload!, but for the save
-    # of Vendor development, this will suffice.
-    def self.reload!
-      begin
-        old_verbose, $VERBOSE = $VERBOSE, nil
-        $".grep(/\/lib\/vendor/).each {|e| $".delete(e) && require(e) }
-      ensure
-        $VERBOSE = old_verbose
-      end
-      "Reloaded!"
-    end
-
-  end
-
-end
+require "vendor/cli/console"
+require "vendor/cli/app"
