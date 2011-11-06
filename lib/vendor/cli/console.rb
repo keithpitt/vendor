25b6c87eabee18399ec939a6a6f65d93ed6bb0d4
Keith Pitt <me@keithpitt.com>
2011-11-06 18:53:05 +1100

    Added a quick way to load example projects in the console

--- lib/vendor/cli/console.rb
+++ lib/vendor/cli/console.rb
@@ -4,6 +4,9 @@ module Vendor
 
     module Console
 
+      RESOURCE_PATH = File.join(Vendor.root, "..", "spec", "support", "resources")
+      PROJECT_RESOURCE_PATH = File.join(RESOURCE_PATH, "projects")
+
       # This reload method kinda works. It works if you change/add methods, but if you
       # remove methods from a class, they still seem to hang around. I believe there is
       # a fair amount of magic in rails that lets you call a reload!, but for the save
@@ -16,6 +19,10 @@ module Vendor
           $VERBOSE = old_verbose
         end
         "Reloaded!"
+      end
+
+      def self.example_project!(name)
+        Vendor::XCode::Project.new(File.join(PROJECT_RESOURCE_PATH, "#{name}/#{name}.xcodeproj"))
       end
 
     end
